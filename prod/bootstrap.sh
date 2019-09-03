567a291534ac0db6b40249606a4e11dfc47f031b
Maximilian Deubel <maximilian.deubel@ovgu.de>
2019-09-03 22:35:44 +0200

    use west for building

--- prod/bootstrap.sh
+++ prod/bootstrap.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-mkdir build
-mkdir build/pre
-mkdir build/coin
-mkdir build/central
-cd    build/pre
-cmake ../../../factory-bonding-onchip
-cd    ../coin
-cmake ../../../coin
-cd    ../central
-cmake ../../../central-onchip
-
+pip3 install cmake ninja west intelhex
+west init ../
+west update
