0c31421f12546649015af014dbca279be03d866d
Maximilian Deubel <maximilian.deubel@gmail.com>
2023-01-03 15:19:42 +0100

    fix flash unlock script

--- flashing/unlock.sh
+++ flashing/unlock.sh
@@ -2,5 +2,5 @@
 raspi-gpio set 27 op dh
 sleep 1
 raspi-gpio set 27 op dl
-openocd -c "source [find interface/raspberrypi-native.cfg]" -f "lift_approtect.ocd"
+openocd -c "source [find interface/raspberrypi-native.cfg]" -f "flash_unprotect.ocd"
 raspi-gpio set 27 op dh
